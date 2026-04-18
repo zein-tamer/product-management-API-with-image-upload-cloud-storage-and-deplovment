c26cbc1be632d512ca9e38bd40e2e357167f7f71
ZEIN TAMER <zaintamer06@gmail.com>
2026-04-18 15:12:55 +0300

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.DATABASE_URI)
+        await mongoose.connect(process.env.MONGO_URI)
 
     }catch(err){
         console.error('Error connecting to mongoDB:', err)
