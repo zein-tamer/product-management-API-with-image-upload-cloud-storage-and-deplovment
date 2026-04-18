aa7186db1900e50ab3263c3c91b00a4d82aeca0a
ZEIN TAMER <zaintamer06@gmail.com>
2026-04-18 16:10:01 +0300

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,19 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        // استخدم DATABASE_URI الذي وضعناه في إعدادات Render
-        const uri = process.env.DATABASE_URI;
-        
-        if (!uri) {
-            throw new Error("رابط قاعدة البيانات (DATABASE_URI) غير معرف في Render!");
-        }
+    try{
+        await mongoose.connect(process.env.DATABASE_URI)
 
-        await mongoose.connect(uri);
-        console.log('✅ MongoDB Connected Successfully to Cloud Atlas');
-    } catch (err) {
-        console.error('❌ Error connecting to mongoDB:', err.message);
-        process.exit(1); 
+    }catch(err){
+        console.error('Error connecting to mongoDB:', err)
+        process.exit(1) 
     }
 }
 
